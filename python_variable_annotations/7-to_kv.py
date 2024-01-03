9df95884211b340faf29f9810fafeb2f94396846
MariamCHARRADA <charradamariam@yahoo.com>
2024-01-03 19:18:57 +0100

    Union

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """returns a tuple of key and square of value"""
-from typing import Tuple
+from typing import Tuple, Union
 
 
-def to_kv(k: str, v: int | float) -> Tuple[str,float]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str,float]:
     """returns a tuple of key and square of value"""
     return (k, v**2)
