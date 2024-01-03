45dd80f0efa5b7df8f3ea90d404ec710f796b86e
MariamCHARRADA <charradamariam@yahoo.com>
2024-01-03 17:44:20 +0100

    format doc

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
-"""takes a list of floats and returns their sum"""
+"""takes a list of floats
+and returns their sum"""
 from typing import List
 
 
 def sum_list(input_list: list[float]) -> float:
-    """takes a list of floats and returns their sum"""
+    """takes a list of floats 
+    and returns their sum"""
     return sum(input_list)
