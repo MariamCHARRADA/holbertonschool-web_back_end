e746634310427b18cadde6e3eba87ed2ca9c32d9
MariamCHARRADA <charradamariam@yahoo.com>
2024-01-03 17:09:28 +0100

    doc format

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
-'''function that takes a float and returns its floor'''
+"""function that takes a float and returns its floor"""
+
 
 def floor(n: float) -> int:
-    '''takes a float and returns its floor'''
+    """takes a float and returns its floor"""
     return int(n)
