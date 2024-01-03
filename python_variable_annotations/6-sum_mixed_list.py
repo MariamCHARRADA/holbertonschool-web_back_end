27559f90e463324f8a2bc56eb48d6163a3d9b761
MariamCHARRADA <charradamariam@yahoo.com>
2024-01-03 17:54:26 +0100

    mixed list

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""returns the sum of a mixed list"""
+from typing import List
+
+
+def sum_mixed_list(mxd_lst: List[int | float]) -> float:
+    """returns the sum of a mixed list"""
+    return sum(mxd_lst)
