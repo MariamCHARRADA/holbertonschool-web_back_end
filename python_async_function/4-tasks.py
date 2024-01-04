7dea725274db7d04e7ed41992aeb641e68c6cbd2
MariamCHARRADA <charradamariam@yahoo.com>
2024-01-05 00:04:51 +0100

    Tasks

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""function that calls wait_random n times"""
+from typing import List
+
+wait_random = __import__("0-basic_async_syntax").wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """function that calls wait_random n times"""
+    delays = [await wait_random(max_delay) for _ in range(n)]
+    return sorted(delays)
