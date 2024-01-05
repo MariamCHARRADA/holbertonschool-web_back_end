ee6bed4f3ea64edc25c103898cdb9f13bbd6ff77
MariamCHARRADA <charradamariam@yahoo.com>
2024-01-05 21:54:55 +0100

    type hint

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,9 +2,10 @@
 """asynchronous generator that yields random numbers"""
 import asyncio
 import random
+from typing import AsyncGenerator
 
 
-async def async_generator():
+async def async_generator() -> AsyncGenerator[float, None]:
     """
     async generator
     """
