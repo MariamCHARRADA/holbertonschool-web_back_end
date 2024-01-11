56e576a5555b5054e821adf7bbe849dc9b0def9b
MariamCHARRADA <charradamariam@yahoo.com>
2024-01-11 20:00:13 +0100

    insert

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -4,6 +4,6 @@
 
 def insert_school(mongo_collection, **kwargs):
     """inserts a new document in a collection based on kwargs"""
-    mongo_collection.insert_one(kwargs)
+    result = mongo_collection.insert_one(kwargs)
 
-    return mongo_collection.inserted_id
+    return result.inserted_id
