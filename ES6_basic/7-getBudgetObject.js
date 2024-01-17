c6d712d28a27e2ee9b4cae1bc77925243d237dc2
MariamCHARRADA <charradamariam@yahoo.com>
2024-01-17 21:36:50 +0100

    property shorthand

--- ES6_basic/7-getBudgetObject.js
+++ ES6_basic/7-getBudgetObject.js
@@ -1,16 +1,8 @@
 export default function getBudgetObject(income, gdp, capita) {
   const budget = {
-    income: export default function getBudgetObject(income, gdp, capita) {
-  const budget = {
     income,
     gdp,
     capita,
-  };
-
-  return budget;
-}income,
-    gdp: gdp,
-    capita: capita,
   };
 
   return budget;
