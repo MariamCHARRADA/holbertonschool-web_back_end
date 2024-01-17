72a4b9747d4ecf7851e550e8fa4e86d3101b9fac
MariamCHARRADA <charradamariam@yahoo.com>
2024-01-17 23:31:07 +0100

    lint

--- ES6_basic/9-getFullBudget.js
+++ ES6_basic/9-getFullBudget.js
@@ -1,7 +1,5 @@
-import getBudgetObject from './7-getBudgetObject.js';
-
 export default function getFullBudgetObject(income, gdp, capita) {
   return {
-    full_income: `$${income}\n${income} euros`
+    full_income: `$${income}\n${income} euros`,
   };
 }
