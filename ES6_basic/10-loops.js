becdab954f00cce037844280509df73a88eda575
MariamCHARRADA <charradamariam@yahoo.com>
2024-01-18 00:14:13 +0100

    fix element

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,6 +1,6 @@
 export default function appendToEachArrayValue(array, appendString) {
   for (let element of array) {
-    let element += appendString;
+    element += appendString;
   }
 
   return array;
