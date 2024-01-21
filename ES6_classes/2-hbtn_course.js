8d85541d73536ccbb94ba8186ebc99f6d76a6324
MariamCHARRADA <charradamariam@yahoo.com>
2024-01-21 16:01:57 +0100

    format

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -10,6 +10,7 @@ export default class HolbertonCourse {
       throw new TypeError('Students must be an array');
     }
 
+
     this._name = name;
     this._length = length;
     this._students = students;
@@ -18,6 +19,7 @@ export default class HolbertonCourse {
   get name() {
     return this._name;
   }
+
   set name(name) {
     if (typeof name !== 'string') {
       throw new TypeError('Name must be a string');
@@ -25,9 +27,11 @@ export default class HolbertonCourse {
     this._name = name;
   }
 
+  
   get length() {
     return this._length;
   }
+  
   set length(length) {
     if (typeof length !== 'number') {
       throw new TypeError('Length must be a number');
