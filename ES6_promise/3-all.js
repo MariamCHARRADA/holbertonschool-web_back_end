01522776a69816906f42ffbe2691b0a40ad74ab5
MariamCHARRADA <charradamariam@yahoo.com>
2024-02-06 22:04:08 +0100

    test task handle multiple successful

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,8 +1,13 @@
-import { uploadPhoto, createUser } from "./utils";
+import  {uploadPhoto}  from './utils';
+import  { createUser }  from './utils';
+
 
 export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()]).then((results) => {
+   Promise.all([uploadPhoto(), createUser()]).then((results) => {
     const [photo, user] = results;
     console.log(photo.body, user.firstName, user.lastName);
+  })
+  .catch((error) => {
+    console.log('Signup system offline');
   });
 }
