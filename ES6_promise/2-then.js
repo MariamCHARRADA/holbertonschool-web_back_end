d618e65aeaad6415f97b5c7a2a90d1918380a420
MariamCHARRADA <charradamariam@yahoo.com>
2024-02-06 21:32:41 +0100

    catch me task

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,12 +1,12 @@
 export default function handleResponseFromAPI(promise) {
   return promise
-    .then((response) => {
-      console.log('Got a response from the API');
-      return response = {
-        status: 200,
-        body: 'success',
-      };
-    })
+  .then(() => {
+    console.log('Got a response from the API');
+    return {
+      status: 200,
+      body: 'success',
+    };
+  })
     .catch(() => {
       return new Error();
     });
