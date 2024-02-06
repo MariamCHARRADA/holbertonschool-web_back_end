844ef79ffca63c9c83338246ce1cad1b82a59caf
MariamCHARRADA <charradamariam@yahoo.com>
2024-02-06 21:19:30 +0100

    passess lint

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,11 +1,12 @@
 export default function handleResponseFromAPI(promise) {
   return promise
     .then(() => {
-      console.log('Got a response from the API');
-      return (response = {
+      response = {
         status: 200,
         body: 'success',
-      });
+      };
+      console.log('Got a response from the API');
+      return response;
     })
     .catch(() => {
       return new Error();
