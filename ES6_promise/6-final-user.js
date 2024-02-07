845eb89fdf726ae3680b8357a7211cc81a03d119
MariamCHARRADA <charradamariam@yahoo.com>
2024-02-07 17:56:38 +0100

    lint

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,8 +1,20 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default async function handleProfileSignup(firstName, lastName, fileName) {
-    const user = { status: 'fulfilled', value: await signUpUser(firstName, lastName).catch(error => error.toString()) };
-    const photo = { status: 'fulfilled', value: await uploadPhoto(fileName).catch(error => error.toString()) };
-    return [user, photo];
-  }
+export default async function handleProfileSignup(
+  firstName,
+  lastName,
+  fileName
+) {
+  const user = {
+    status: 'fulfilled',
+    value: await signUpUser(firstName, lastName).catch((error) =>
+      error.toString()
+    ),
+  };
+  const photo = {
+    status: 'fulfilled',
+    value: await uploadPhoto(fileName).catch((error) => error.toString()),
+  };
+  return [user, photo];
+}
