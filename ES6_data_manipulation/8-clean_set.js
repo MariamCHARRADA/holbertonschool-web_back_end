1f9189df7a049a9f46b009bfaaf7349be1f0b0c3
MariamCHARRADA <charradamariam@yahoo.com>
2024-02-13 12:59:53 +0100

    💩 lint

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,13 @@
 export default function cleanSet(set, startString) {
   let cleaned = '';
-  set.forEach((element) => {
-    if (element.startsWith(startString) && typeof startString === 'string') {
-      cleaned += element.slice(startString.length) + '-';
-    }
-  });
-  cleaned = cleaned.slice(0, -1);
+  if(typeof(startString) === 'string'){
+    set.forEach((element) => {
+        if (element.startsWith(startString)) {
+          cleaned += element.slice(startString.length) + '-';
+        }
+      });
+      cleaned = cleaned.slice(0, -1);
+  }
+
   return cleaned;
 }
