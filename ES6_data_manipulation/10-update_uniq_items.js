80edc6adee440152fc6735b13dbe90f31846f5b9
MariamCHARRADA <charradamariam@yahoo.com>
2024-02-13 19:32:10 +0100

    try catch

--- ES6_data_manipulation/10-update_uniq_items.js
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -1,7 +1,11 @@
 export default function updateUniqueItems(itemList) {
-  itemList.forEach((quantity, item) => {
-    if (quantity === 1) {
-      itemList.set(item, 100);
-    }
-  });
+  try {
+    itemList.forEach((quantity, item) => {
+      if (quantity === 1) {
+        itemList.set(item, 100);
+      }
+    });
+  } catch (error) {
+    console.error('Cannot process');
+  }
 }
