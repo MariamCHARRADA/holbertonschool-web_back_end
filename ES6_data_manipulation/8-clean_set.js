c7aa20022d63e9a7741ce43aed9a87e7e3f868f9
MariamCHARRADA <charradamariam@yahoo.com>
2024-02-13 13:05:11 +0100

    💄 💄 💄

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -3,7 +3,8 @@ export default function cleanSet(set, startString) {
   if (typeof startString === 'string') {
     set.forEach((element) => {
       if (element.startsWith(startString)) {
-        cleaned += element.slice(startString.length) + '-';
+        cleaned += element.slice(startString.length);
+        cleaned += '-';
       }
     });
     cleaned = cleaned.slice(0, -1);
