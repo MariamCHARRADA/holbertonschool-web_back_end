4c78aed3f90f791591735e2b49643042fca940d6
MariamCHARRADA <charradamariam@yahoo.com>
2024-06-20 20:25:13 +0100

    Regex-ing done!

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -3,10 +3,11 @@
 
 import re
 
+
 def filter_datum(fields, redaction, message, separator):
     """returns the log message obfuscated"""
     for field in fields:
-        pattern = rf"{field}=[^{re.escape(separator)}]*"
+        pattern = f"{field}=[^{separator}]+"
         replacement = f"{field}={redaction}"
         message = re.sub(pattern, replacement, message)
     return message
