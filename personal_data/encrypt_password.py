e20ead35261c91a70e880fc5c1c1c26c3423c486
MariamCHARRADA <charradamariam@yahoo.com>
2024-06-21 23:50:15 +0100

    Check valid password

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -8,3 +8,9 @@ def hash_password(password: str) -> bytes:
     """encrypts a password"""
     p_bytes = password.encode()
     return bcrypt.hashpw(p_bytes, bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """check if password is valid"""
+    p_bytes = password.encode()
+    return bcrypt.checkpw(p_bytes, hashed_password)
