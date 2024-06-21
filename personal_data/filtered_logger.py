4659772751ace965f4bf6b6aa832b978490763ce
MariamCHARRADA <charradamariam@yahoo.com>
2024-06-21 13:56:35 +0100

    Log formatter built-in

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -14,15 +14,15 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
-        """constructor"""
+        """initializes the RedactingFormatter"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
-
 
     def format(self, record: logging.LogRecord) -> str:
         """formats records and filters sensitive info"""
         record.msg = filter_datum(
-            self.fields, self.REDACTION, record.msg, self.SEPARATOR)
+            self.fields, self.REDACTION, record.msg, self.SEPARATOR
+        )
         return super().format(record)
 
 
