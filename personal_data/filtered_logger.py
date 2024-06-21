52e6906e067cf349906229d10909573240208ddd
MariamCHARRADA <charradamariam@yahoo.com>
2024-06-21 17:54:56 +0100

    remove class

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -6,24 +6,7 @@ from typing import List
 import logging
 
 
-class RedactingFormatter(logging.Formatter):
-    """Redacting Formatter class that inherits from built-in (parent class) logging.Formatter"""
 
-    REDACTION = "***"
-    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPARATOR = ";"
-
-    def __init__(self, fields: List[str]):
-        """initializes the RedactingFormatter"""
-        super(RedactingFormatter, self).__init__(self.FORMAT)
-        self.fields = fields
-
-    def format(self, record: logging.LogRecord) -> str:
-        """formats records and filters sensitive info"""
-        record.msg = filter_datum(
-            self.fields, self.REDACTION, record.msg, self.SEPARATOR
-        )
-        return super().format(record)
 
 
 
