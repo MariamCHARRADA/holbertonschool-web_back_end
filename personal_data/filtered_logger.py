2100e5ac7d6f97a5d2e80b55a920b59d0d4dce5a
MariamCHARRADA <charradamariam@yahoo.com>
2024-06-21 23:31:23 +0100

    Read and filter data

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -73,3 +73,20 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
         database=db,
     )
     return connection
+
+
+def main() -> None:
+    """reads and filters data"""
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users;")
+    logger = get_logger()
+
+    for row in cursor.fetchall():
+        message = f"name={row[0]}; email={row[1]}; phone={row[2]}; ssn={row[3]}; password={row[4]}; ip={row[5]}; last_login={row[6]}; user_agent={row[7]};"
+        logger.info(message)
+
+    cursor.close()
+    db.close()
+    if __name__ == "__main__":
+        main()
