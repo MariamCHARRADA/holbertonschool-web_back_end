5a1cedbd6b6ee666f3b9514f3c0f24b4d79279d2
MariamCHARRADA <charradamariam@yahoo.com>
2024-06-21 13:52:18 +0100

    fix filter_datum

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -15,8 +15,9 @@ class RedactingFormatter(logging.Formatter):
 
     def __init__(self, fields: List[str]):
         """constructor"""
-        self.fields = fields
         super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
 
     def format(self, record: logging.LogRecord) -> str:
         """formats records and filters sensitive info"""
@@ -30,7 +31,7 @@ def filter_datum(
 ) -> str:
     """returns the log message obfuscated"""
     for field in fields:
-        pattern = f"{field}=[^{separator}]+"
+        pattern = f"{field}=[^{separator}]*"
         replacement = f"{field}={redaction}"
         message = re.sub(pattern, replacement, message)
     return message
