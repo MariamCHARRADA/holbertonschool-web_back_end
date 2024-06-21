f7fb85f380b8976cbfa3e9f7e291e7af69c44abe
MariamCHARRADA <charradamariam@yahoo.com>
2024-06-21 22:34:57 +0100

    return object

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -59,7 +59,7 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db():
+def get_db() -> mysql.connector.connection.MySQLConnection:
     """returns a connector to the db"""
     username = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
     password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
