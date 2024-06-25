b3d5b66042f82f807dc0b532aed3bcfacac0def7
MariamCHARRADA <charradamariam@yahoo.com>
2024-06-25 19:29:59 +0100

    add slash

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -10,8 +10,10 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """determines if the path requires authentication"""
-        if path is None or excluded_paths is None or not len(excluded_paths):
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
             return True
+        if path[-1] != "/":
+            path += "/"
         if path in excluded_paths:
             return False
         return True
