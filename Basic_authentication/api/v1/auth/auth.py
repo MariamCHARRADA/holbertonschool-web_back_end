b2338d7ad559b7a3154bb210ad2127f7394d744b
MariamCHARRADA <charradamariam@yahoo.com>
2024-06-25 19:11:39 +0100

    Auth class

---
+++ Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""auth.py"""
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """class to manage the API authentication"""
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """determines if the path requires authentication"""
+        return False
+
+    def authorization_header(self, request=None) -> str:
+        """retrieves auth header from the request"""
+        return None
+
+    def current_user(self, request=None) -> TypeVar("User"):
+        """retrieves the current user from the request"""
+        return None
