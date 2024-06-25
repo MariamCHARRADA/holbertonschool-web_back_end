9b6d9513d029ba2c3accadafe34467e21cd4c7c8
MariamCHARRADA <charradamariam@yahoo.com>
2024-06-25 19:16:58 +0100

    test

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -10,7 +10,9 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """determines if the path requires authentication"""
-        return False
+        if path in excluded_paths:
+            return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """retrieves auth header from the request"""
