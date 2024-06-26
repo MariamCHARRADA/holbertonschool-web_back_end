7f4a118ba49b244db1d8d02054f62858ec442da8
MariamCHARRADA <charradamariam@yahoo.com>
2024-06-26 23:42:54 +0100

    format fix

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,7 +9,8 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """Basic Authentication class"""
 
-    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
         """returns the Base64 part of the Authorization header"""
         if authorization_header is None:
             return None
@@ -18,4 +19,5 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         else:
-            return authorization_header[len("Basic ") :]
+
+            return authorization_header[len("Basic "):]
