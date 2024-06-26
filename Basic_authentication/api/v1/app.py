dfb4f0c952cd50299816e657c0bb91e0fe2c48d8
MariamCHARRADA <charradamariam@yahoo.com>
2024-06-26 12:01:36 +0100

    if is none

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -49,9 +49,9 @@ def before_request():
 
     if not auth.require_auth(request.path, excluded_paths):
         return
-    if not auth.authorization_header(request):
+    if auth.authorization_header(request) is None:
         abort(401)
-    if not auth.current_user(request):
+    if auth.current_user(request) is None:
         abort(403)
 
 
