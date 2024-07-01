e3e8eccb938d29613ca0a3f2222dc73b088a6da1
MariamCHARRADA <charradamariam@yahoo.com>
2024-07-01 16:06:00 +0100

    Before request

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -52,7 +52,8 @@ def before_request():
     excluded_paths = [
         "/api/v1/status/",
         "/api/v1/unauthorized/",
-        "/api/v1/forbidden/"]
+        "/api/v1/forbidden/",
+        "/api/v1/auth_session/login/"]
 
     if not auth.require_auth(request.path, excluded_paths):
         return
@@ -60,6 +61,9 @@ def before_request():
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
+    if auth.authorization_header(
+            request) is None and auth.session_cookie(request) is None:
+        abort(401)
 
     request.current_user = auth.current_user(request)
 
