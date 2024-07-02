4a8bb3eb1493dbb91712195da592fb4bed2ccc20
MariamCHARRADA <charradamariam@yahoo.com>
2024-07-02 18:31:45 +0100

    view fix

--- Session_authentication/api/v1/views/session_auth.py
+++ Session_authentication/api/v1/views/session_auth.py
@@ -7,11 +7,11 @@ from models.user import User
 from os import getenv
 
 
-@app_views.route('api/v1/views/session_auth.py',
+@app_views.route("api/v1/views/session_auth.py",
                  methods=[POST], strict_slashes=False)
 def login():
-    """ returns logged user"""
-    from api.v1.auth.auth import Auth
+    """returns logged user"""
+    from api.v1.app import auth
 
     email = request.form.get("email")
     password = request.form.get("password")
@@ -25,17 +25,15 @@ def login():
     user = User.search({"email": email})
     if not user:
         return jsonify({"error": "no user found for this email"}), 404
-    if not user.is_valid_password(password):
-        return jsonify({"error": "wrong password"}), 401
-
-    from api.v1.app import auth
 
     userr = user[0]
+    if not userr.is_valid_password(password):
+        return jsonify({"error": "wrong password"}), 401
+
     session_id = auth.create_session(userr.id)
-
     SESSION_NAME = getenv("SESSION_NAME")
-
     response = jsonify(userr.to_json())
+
     response.set_cookie(SESSION_NAME, session_id)
 
     return response
