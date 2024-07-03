3be91cca558a1bc561c12b7d68d3c728a546c9b4
MariamCHARRADA <charradamariam@yahoo.com>
2024-07-03 18:06:12 +0100

    indent fix

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -68,13 +68,13 @@ class TestGithubOrgClient(TestCase):
             )
             mock_public_repos_url.assert_called_once_with()
 
-        @parameterized.expand([
-            ({"license": {"key": "my_license"}}, "my_license", True),
-            ({"license": {"key": "other_license"}}, "my_license", False),
-        ])
-        def test_has_license(self, repo, license_key, expected):
-            """test license"""
-            github_client = GithubOrgClient()
-            result = github_client.has_license(repo, license_key)
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False),
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        """test license"""
+        github_client = GithubOrgClient()
+        result = github_client.has_license(repo, license_key)
 
-            self.assertEqual(result, expected)
+        self.assertEqual(result, expected)
