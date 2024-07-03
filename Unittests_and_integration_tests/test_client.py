8861db1a66e22291d267913bfd3b94a68b0645cf
MariamCHARRADA <charradamariam@yahoo.com>
2024-07-03 17:10:27 +0100

    More patching

--- Unittests_and_integration_tests/test_client.py
+++ Unittests_and_integration_tests/test_client.py
@@ -25,11 +25,32 @@ class TestGithubOrgClient(TestCase):
         mock_get_json.assert_called_once_with(f"https://api.github.com/orgs/{org_name}")
         self.assertEqual(result, expected_value)
 
-        def test_public_repos_url(self, mock_org):
-            """test public_repos_url"""
-            mock_org.return_value = {
-                "repos_url": "https://api.github.com/orgs/google/repos"
-            }
-            client = GithubOrgClient("google")
-            result = client.public_repos_url()
-            self.assertEqual(result, "https://api.github.com/orgs/google/repos")
+    def test_public_repos_url(self, mock_org):
+        """test public_repos_url"""
+        mock_org.return_value = {
+            "repos_url": "https://api.github.com/orgs/google/repos"
+        }
+        client = GithubOrgClient("google")
+        result = client.public_repos_url()
+        self.assertEqual(result, "https://api.github.com/orgs/google/repos")
+
+    @patch("client.GithubOrgClient.get_json")
+    def test_public_repos(self, mock_get_json):
+        """ test public_repos"""
+        mock_get_json.return_value = [
+            { "name": "repo1"}
+            {"name": "repo2"}
+        ]
+        
+        with patch('client.GithubOrgClient._public_repos_url', new_callable=unittest.mock.PropertyMock) as mock_public_repos_url:
+            mock_public_repos_url.return_value = "https://api.github.com/orgs/test_org/repos"
+            client = GithubOrgClient("test_org")
+            
+            result = client.public_repos()
+            
+            self.assertEqual(result, ["repo1", "repo2"])
+            
+            mock_public_repos_url.assert_called_once()
+            
+            mock_get_json.assert_called_once_with("https://api.github.com/orgs/test_org/repos")
+            
