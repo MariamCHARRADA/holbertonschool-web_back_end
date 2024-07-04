c392ca2aeba39df98601664813e9e5f753849ca8
MariamCHARRADA <charradamariam@yahoo.com>
2024-07-04 16:45:38 +0100

    type hint

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -5,6 +5,7 @@ from flask import Flask, render_template, request
 from flask_babel import Babel
 
 app = Flask(__name__)
+babel = Babel(app)
 
 
 class Config:
@@ -16,11 +17,9 @@ class Config:
 
 app.config.from_object(Config)
 
-babel = Babel(app)
-
 
 @babel.localeselector
-def get_locale():
+def get_locale() -> str:
     """ determines the best match with our supported languages"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
