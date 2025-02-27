eea9efc66513bf429cf6b8d801a7530effbb8417
MariamCHARRADA <charradamariam@yahoo.com>
2025-02-27 23:32:47 +0100

    Register user

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -3,8 +3,11 @@
 Flask app
 """
 from flask import Flask, jsonify
+from auth import Auth
+
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/", methods=["GET"])
@@ -12,6 +15,21 @@ def welcome():
     """Return a welcome message"""
     return jsonify({"message": "Bienvenue"})
 
+@app.route("/users", methods=["POST"])
+def users():
+    """Return a list of users"""
+    email = request.form.get("email")
+    password = request.form.get("password")
 
+    if not email or not password:
+        abort(400, description="Missing email or password")
+
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"}), 200
+    except Exception:
+        return jsonify({"message": "email already registered"})
+    
+    
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
