ed085a2afaf3f6ca88ebbac5fbd88e0255c621be
MariamCHARRADA <charradamariam@yahoo.com>
2025-02-27 23:36:54 +0100

    fix code

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -15,21 +15,19 @@ def welcome():
     """Return a welcome message"""
     return jsonify({"message": "Bienvenue"})
 
+
 @app.route("/users", methods=["POST"])
 def users():
     """Return a list of users"""
     email = request.form.get("email")
     password = request.form.get("password")
 
-    if not email or not password:
-        abort(400, description="Missing email or password")
-
     try:
-        user = AUTH.register_user(email, password)
-        return jsonify({"email": user.email, "message": "user created"}), 200
+        AUTH.register_user(email, password)
+        return jsonify({"email": email, "message": "user created"}), 200
     except Exception:
         return jsonify({"message": "email already registered"})
-    
-    
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
