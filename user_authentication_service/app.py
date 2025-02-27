0b3c460b129e8d74b2e038aaaf679156362740c7
MariamCHARRADA <charradamariam@yahoo.com>
2025-02-28 00:48:59 +0100

    update password endpoint

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -81,5 +81,18 @@ def get_reset_password_token():
         abort(403)
 
 
+@app.route("/reset_password", methods=["PUT"])
+def update_password():
+    """update password"""
+    email = request.form.get("email")
+    password = request.form.get("password")
+    reset_token = request.form.get("reset_token")
+    try:
+        AUTH.update_password(password, reset_token)
+        return jsonify({"email": email, "message": "Password updated"})
+    except Exception:
+        abort(403)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
