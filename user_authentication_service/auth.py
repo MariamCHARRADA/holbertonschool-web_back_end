94383015885334895d5fedadf8ebc251fed980e2
MariamCHARRADA <charradamariam@yahoo.com>
2025-02-28 00:16:36 +0100

    logout

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -60,3 +60,10 @@ class Auth:
             return user
         except NoResultFound:
             return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """Destroys a user's session"""
+        try:
+            self._db.update_user(user_id, session_id=None)
+        except NoResultFound:
+            pass
