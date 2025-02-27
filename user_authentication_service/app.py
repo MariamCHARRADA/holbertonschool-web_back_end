06144940cc2c73bb3ecf55336b4388804eabc61c
MariamCHARRADA <charradamariam@yahoo.com>
2025-02-28 00:08:57 +0100

    login

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -29,5 +29,20 @@ def users():
         return jsonify({"message": "email already registered"})
 
 
+@app.route("/sessions", methods=["POST"])
+def login():
+    """login"""
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        response = jsonify({"email": email, "message": "logged in"})
+        response.set_cookie("session_id", session_id)
+        return response
+    else:
+        abort(401)
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
