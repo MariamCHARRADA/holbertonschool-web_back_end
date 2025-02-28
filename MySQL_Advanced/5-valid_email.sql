6ab3c2a1d579aa006c08d132bb5c70d94de06b5a
MariamCHARRADA <charradamariam@yahoo.com>
2025-02-28 08:47:25 +0100

    valid email

---
+++ MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,13 @@
+-- SQL script to create a trigger
+DELIMITER $$
+
+CREATE TRIGGER new_email
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF OLD.email != NEW.email THEN
+    SET NEW.valid_email = 0 ADD
+    END IF;
+END$$
+
+DELIMITER;
