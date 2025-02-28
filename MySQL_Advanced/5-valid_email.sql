f78627d9eb9318bf8b4dbfa747c81f0b5a946ffc
MariamCHARRADA <charradamariam@yahoo.com>
2025-02-28 09:41:20 +0100

    hot fix

--- MySQL_Advanced/5-valid_email.sql
+++ MySQL_Advanced/5-valid_email.sql
@@ -1,12 +1,12 @@
 -- SQL script to create a trigger
 DELIMITER $$
 
-CREATE TRIGGER new_email
-BEFORE UPDATE ON users
+CREATE TRIGGER `new_email`
+BEFORE UPDATE ON `users`
 FOR EACH ROW
 BEGIN
     IF OLD.email != NEW.email THEN
-        SET NEW.valid_email = 0
+        SET NEW.valid_email = 0;
     END IF;
 END$$
 
