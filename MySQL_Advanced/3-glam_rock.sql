6b6d4bb87a419621ac8932d46d83e71e321c6404
MariamCHARRADA <charradamariam@yahoo.com>
2025-02-28 08:37:46 +0100

    glam rock

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,8 +1,5 @@
--- Lists all bands with Glam rock as their main style, ranked by their longevity.
--- Column names: band_name and lifespan (in years)
--- Lifespan is calculated using the attributes `formed` and `split`.
+-- Lists all bands with Glam rock as their main style, ranked by their longevity
 
-SELECT band_name, COALESCE(split, 2020) - formed as lifespan
-FROM metal_bands
+SELECT band_name, COALESCE(split, 2020) - formed as lifespan FROM metal_bands
 WHERE
     style LIKE '%Glam rock%'
