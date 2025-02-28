4f636e258e142341bae57bb786c914924e7ef0b7
MariamCHARRADA <charradamariam@yahoo.com>
2025-02-28 08:34:57 +0100

    BUY

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -2,10 +2,7 @@
 -- Column names: band_name and lifespan (in years)
 -- Lifespan is calculated using the attributes `formed` and `split`.
 
-SELECT band_name, IF(
-        split IS NOT NULL, split - formed, 2022 - formed
-    ) AS lifespan
+SELECT band_name, COALESCE(split, 2020) - formed as lifespan
 FROM metal_bands
 WHERE
     style LIKE '%Glam rock%'
-ORDER BY lifespan DESC;
