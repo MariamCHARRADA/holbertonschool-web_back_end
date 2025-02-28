c3106b57e883be276a3dfd9408487f7c445e9779
MariamCHARRADA <charradamariam@yahoo.com>
2025-02-28 09:38:29 +0100

    typo fix

--- MySQL_Advanced/11-need_meeting.sql
+++ MySQL_Advanced/11-need_meeting.sql
@@ -4,5 +4,5 @@ DROP VIEW IF EXISTS need_meeting;
 CREATE VIEW need_meeting AS 
 SELECT name
 FROM students
-WHERE score > 80
+WHERE score < 80
 AND last_meeting IS NULL OR DATE(NOW()) > DATE_ADD(last_meeting, INTERVAL 1 MONTH);
