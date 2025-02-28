cc459105b31fa9a2c9b236839cfcd0ae87efeaef
MariamCHARRADA <charradamariam@yahoo.com>
2025-02-28 08:25:27 +0100

    In and not out

---
+++ MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,13 @@
+-- Creates a table named 'users' with the following attributes:
+-- id: integer, never null, auto increment, primary key
+-- email: string (255 characters), never null, unique
+-- name: string (255 characters)
+-- country: enumeration of countries (US, CO, TN), never null, default is US
+-- If the table already exists, the script should not fail
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
